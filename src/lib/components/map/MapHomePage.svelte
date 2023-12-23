7716144595d42763322ad2bb894d4b8aefb9ce57
ellsphillips <elliott.phillips.dev@gmail.com>
2023-12-23 18:22:45 +0000

    Enhance: sidebar placeholder content

---
+++ src/lib/components/map/MapHomePage.svelte
@@ -0,0 +1,12 @@
+<h1>Atlas</h1>
+
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a suscipit nunc, ac sollicitudin
+	ipsum. Curabitur tincidunt auctor ante, vitae laoreet enim rhoncus non. In sit amet dapibus magna.
+	Nullam scelerisque magna non elit dapibus imperdiet. Nulla eget pharetra ante. Mauris feugiat eros
+	sed nibh euismod, ut maximus sem tristique. Sed tincidunt vel ligula at rutrum. Donec consequat
+	augue vel nisi semper aliquet. Curabitur id vulputate arcu. Morbi porta ut tellus a elementum.
+	Proin pulvinar fermentum est et finibus. Quisque faucibus arcu ac pulvinar imperdiet. Mauris ac
+	dui enim. Donec sodales tortor quis nulla euismod ullamcorper. Sed fringilla laoreet arcu, ac
+	pellentesque elit placerat vitae. Donec lacinia cursus scelerisque.
+</p>
