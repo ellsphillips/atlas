126704b1ee2f90a9461f7a0bb5d08df9c97d6b83
ellsphillips <elliott.phillips.dev@gmail.com>
2023-12-24 12:11:40 +0000

    Enhance: boundary types

---
+++ src/lib/types.ts
@@ -0,0 +1,17 @@
+import type { FeatureCollection, Polygon } from 'geojson';
+
+export interface GeoJSONProperties<T> {
+	GlobalID: string;
+	ObjectID: number;
+	bng_e: number;
+	bng_n: number;
+	code: T extends `${string}cd` ? string : never;
+	name: T extends `${string}nm` ? string : never;
+	welsh_name: T extends `${string}nmw` ? string : never;
+	lat: number;
+	long: number;
+}
+
+export type BoundaryData = FeatureCollection<Polygon, GeoJSONProperties<GeoType>>;
+
+export type GeoType = 'country' | 'region';
