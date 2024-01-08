81939442027056870840db3801df56eb07caf462
ellsphillips <elliott.phillips.dev@gmail.com>
2024-01-08 15:44:00 +0000

    Feature: sidebar sample icons

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,7 @@
+import Chart from './Chart.svelte';
+import Database from './Database.svelte';
+import Globe from './Globe.svelte';
+import SearchText from './SearchText.svelte';
+import TimelineClock from './TimelineClock.svelte';
+
+export default [Chart, Database, Globe, SearchText, TimelineClock];
